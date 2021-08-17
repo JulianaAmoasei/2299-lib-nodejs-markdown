88c5ecb7ca18f52a71ed500dfef40fb1387e06f7
Juliana Amoasei <juliana.amoasei@gmail.com>
2021-08-17 16:41:08 -0300

    video 1.3

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('vamos começar!');
+const chalk = require('chalk');
+
+console.log(chalk.blue('vamos começar!'));
+
+const paragrafo = 'Texto retornado por uma função';
+
+function texto(string) {
+  return string;
+}
+
+console.log(texto(paragrafo));
