43dab41d92b25badf095cba1fe706e6cba7212d8
Juliana Amoasei <juliana.amoasei@gmail.com>
2021-08-17 20:13:22 -0300

    vídeo 2.2

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
+}
+
 function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
-  fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+    if (erro) {
+      trataErro(erro);
+    }
     console.log(chalk.green(texto));
   })
 }
