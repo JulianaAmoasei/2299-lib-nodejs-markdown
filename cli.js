fc4c00099f51ad9b1af734059ca76f39438a3274
Juliana Amoasei <juliana.amoasei@gmail.com>
2021-08-19 20:41:05 -0300

    vídeo 4.2

--- cli.js
+++ cli.js
@@ -1,5 +1,11 @@
+const chalk = require('chalk');
 const pegaArquivo = require('./index');
 
 const caminho = process.argv;
 
-console.log(pegaArquivo(caminho[2]));
+async function processaTexto(caminhoDeArquivo) {
+  const resultado = await pegaArquivo(caminhoDeArquivo[2]);
+  console.log(chalk.yellow('lista de links'), resultado);
+}
+
+processaTexto(caminho);
