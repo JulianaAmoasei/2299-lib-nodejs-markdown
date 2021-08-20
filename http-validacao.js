4b635c5ef6c44480827a9db05a1673af9862a050
Juliana Amoasei <juliana.amoasei@gmail.com>
2021-08-20 19:37:00 -0300

    vídeo 5.2

--- http-validacao.js
+++ http-validacao.js
@@ -1,5 +1,12 @@
+function geraArrayDeURLs(arrayLinks) {
+  //loop para cada { chave: valor }
+  // objeto -> [valor]
+  //Object.values(objeto)
+  return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
+}
+
 function validaURLs(arrayLinks) {
-  return arrayLinks;
+  return geraArrayDeURLs(arrayLinks);
 }
 
 module.exports = validaURLs;
