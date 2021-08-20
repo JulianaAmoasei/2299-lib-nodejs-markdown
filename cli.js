a5d709b57725339688cb9ba2acf4d0bdc3b52690
Juliana Amoasei <juliana.amoasei@gmail.com>
2021-08-20 20:56:31 -0300

    vídeo 5.3

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const caminho = process.argv;
 async function processaTexto(caminhoDeArquivo) {
   const resultado = await pegaArquivo(caminhoDeArquivo[2]);
   if (caminho[3] === 'validar') {
-    console.log(chalk.yellow('links validados'), validaURLs(resultado));
+    console.log(chalk.yellow('links validados'), await validaURLs(resultado));
   } else {
     console.log(chalk.yellow('lista de links'), resultado);
   }
--- http-validacao.js
+++ http-validacao.js
@@ -1,12 +1,22 @@
+const fetch = require('node-fetch');
+
+async function checaStatus(arrayURLs) {
+  //promises async await
+  const arrayStatus = await Promise.all(arrayURLs.map(async url => {
+    const res = await fetch(url)
+    return res.status;
+  }))
+  return arrayStatus;
+}
+
 function geraArrayDeURLs(arrayLinks) {
-  //loop para cada { chave: valor }
-  // objeto -> [valor]
-  //Object.values(objeto)
   return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
 }
 
-function validaURLs(arrayLinks) {
-  return geraArrayDeURLs(arrayLinks);
+async function validaURLs(arrayLinks) {
+  const links = geraArrayDeURLs(arrayLinks);
+  const statusLinks = await checaStatus(links);
+  return statusLinks;
 }
 
 module.exports = validaURLs;
