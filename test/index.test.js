7ab53ec34f069ec04e7e088b93dbf73be7248f95
Juliana Amoasei <juliana.amoasei@gmail.com>
2021-08-23 16:53:11 -0300

    vídeo 6.2

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,17 @@
 const pegaArquivo = require('../index');
 
-test('deve ser uma função', () => {
-  expect(typeof pegaArquivo).toBe('function');
-});
+const arrayResult = [
+  {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+  }
+]
+
+describe('pegaArquivo::', () => {
+  it('deve ser uma função', () => {
+    expect(typeof pegaArquivo).toBe('function');
+  })
+  it('deve retornar array com resultados', async () => {
+    const resultado = await pegaArquivo('/home/juliana/Documents/alura/lib-markdown/test/arquivos/texto1.md') 
+    expect(resultado).toEqual(arrayResult)
+  })
+})
